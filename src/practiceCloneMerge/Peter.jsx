fac74e369eafcda49a2f4ec6d67c0c0603b8baf5
unkerpete <118168304+unkerpete@users.noreply.github.com>
2023-01-30 20:46:32 +0800

    for practicing merging

---
+++ src/practiceCloneMerge/Peter.jsx
@@ -0,0 +1,7 @@
+// This comment is for practicing
+// 1. Clone of main branch.
+// 2. Create a sub-branch for each feature you work on.
+// 3. After you complete your feature, merge this sub-branch to your own clone.
+// 4. Pull origin(if any) from master branch. This is to ensure your clone is up to date first before submitting a pull request of your recently completed sub-branch
+// 5. Submit pull request of your clone so that your sub-branch can be submitted to main branch.
+// 6. Done, and repeat step 2 to 5 for next feature you have to work on
