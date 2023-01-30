84e7e19c4ba623f25688938146253264e7d77044
unkerpete <peterkjx@gmail.com>
2023-01-30 11:33:17 +0800

    Initial commit

---
+++ src/pages/GetInvolved.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GetInvolved = () => {
+  return <div></div>;
+};
+
+export default GetInvolved;
---
+++ src/pages/Happening.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Happening = () => {
+  return <div></div>;
+};
+
+export default Happening;
