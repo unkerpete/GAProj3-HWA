186fa3dbffecef576db5ae603766a0a85b8d2302
cshujun23 <c.shujun23@gmail.com>
2023-02-01 00:01:22 +0800

    home page initial commit

---
+++ src/Components/categoryCardData.js
@@ -0,0 +1,24 @@
+const categoryCardData = [
+  {
+    img: "",
+    subheader: "For Caregivers",
+    body: "We support family caregivers through providing regular quality programmes and services, and uplifts them with a robust support system.",
+  },
+  {
+    img: "",
+    subheader: "For Donors",
+    body: "Your gift will enable and support individuals with physical challenges as well as their family caregivers through HWA's rehabilitaion, home care, and transport programmes and services.",
+  },
+  {
+    img: "",
+    subheader: "For Volunteers",
+    body: "The service of volunteers contributes significantly to the social reintegration of the physicallyy challenged in our community.",
+  },
+  {
+    img: "",
+    subheader: "For People with Disabilities",
+    body: "HWA's rehabilitation, home care, and transportation services caters to the distinct needs of the physically challenged. No one gets left behind",
+  },
+];
+
+export default categoryCardData;
