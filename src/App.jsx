310c04c64659ce046826ae9cacff5cc93c931709
unkerpete <peterkjx@gmail.com>
2023-02-01 13:01:40 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
-import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className='App'>Hello Team</div>;
+  return (
+    <div className=''>
+      <NavBar />
+
+      <Routes>
+        <Route path='/' element={<Navigate replace to='/Home' />} />
+        <Route path='/page-one' element={<PageOne />} />
+        <Route path='/page-one/:item' element={<Details />} />
+        <Route path='/page-two/' element={<PageTwo />} />
+        <Route path='/page-three/' element={<PageThree />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
