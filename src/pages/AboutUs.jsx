9c00c7def2a414f2244d4df82c86fd24380b7836
tyler <tyler.tongseng@gmail.com>
2023-02-02 13:36:16 +0800

    tailwind stylesheet

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React from "react";
 
 const AboutUs = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="bg-primary-800 text-gray-400">hi</h1>
+      <h1 className="bg-primary-600 text-gray-400">hi</h1>
+      <h1 className="bg-primary-400 text-gray-400">hi</h1>
+      <h1 className="bg-primary-200 text-gray-400">hi</h1>
+      <h1 className="bg-white text-gray-400">hi</h1>
+      <h1 className="bg-secondary-600 text-gray-400">hi</h1>
+      <h1 className="bg-secondary-400 text-gray-400">hi</h1>
+      <h1 className="bg-secondary-200 text-gray-400">hi</h1>
+      <h1 className="bg-secondary-200 text-primary-800 border-primary-800">
+        hi
+      </h1>
+    </div>
+  );
 };
 
 export default AboutUs;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          800: "#2B4435",
+          600: "#4F7F71",
+          400: "#99B8A3",
+          200: "#D0DFD5",
+        },
+        secondary: {
+          600: "#C7CBD0",
+          400: "#E3EAF3",
+          200: "#F3F8FF",
+        },
+      },
+    },
   },
   plugins: [],
 };
+
+//eg. bg-primary-800 // text-primary-800 // bg-primary-800
