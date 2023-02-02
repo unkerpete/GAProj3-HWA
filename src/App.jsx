2a6cd0c7eacefb38c3bc644c30ddd859db7e12ba
cshujun23 <c.shujun23@gmail.com>
2023-02-02 11:16:54 +0800

    Update App initial route to Home

--- src/App.jsx
+++ src/App.jsx
@@ -15,9 +15,9 @@ function App() {
       <NavBar />
 
       <Routes>
-        <Route path="/" element={<Navigate replace to="/about-us" />} />
+        <Route path="/" element={<Navigate replace to="/Home" />} />
         <Route path="/Home" element={<Home />} />
-        
+
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/our-services" element={<OurServices />} />
         <Route path="/get-involved/" element={<GetInvolved />} />
