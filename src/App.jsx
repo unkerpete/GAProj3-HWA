c970f42c7fc460e4ed417001700a2a35fe08ce29
cshujun23 <c.shujun23@gmail.com>
2023-02-02 11:03:39 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return <div className='App'>Hello Team</div>;
+  return (
+    <div className="">
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/about-us" />} />
+        <Route path="/Home" element={<Home />} />
+
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/our-services" element={<OurServices />} />
+        <Route path="/get-involved/" element={<GetInvolved />} />
+        <Route path="/current-events/" element={<CurrentEvents />} />
+        <Route path="/contact-us/" element={<ContactUs />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
