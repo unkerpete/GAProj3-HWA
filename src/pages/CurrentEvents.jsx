3069946cedda5a71f2335a231b2c2bbafe9ce0a4
eezhar <eezhar@gmail.com>
2023-02-02 10:56:28 +0800

    update routes

--- src/pages/CurrentEvents.jsx
+++ src/pages/CurrentEvents.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const AboutOurPeople = () => {
+const CurrentEvents = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const AboutOurPeople = () => {
     );
 };
 
-export default AboutOurPeople;
+export default CurrentEvents;
