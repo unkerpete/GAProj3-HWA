a84890edc631342cd74d9319773000e88682d21a
unkerpete <peterkjx@gmail.com>
2023-02-04 16:20:08 +0800

    Minor styling changes. ISSUE: added logo that isn't rendering

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div className='bg-peach'>
-      <div className='h-24 flex'>
-        <div className='mr-80 pt-4 pl-20'>Logo</div>
+    <div className='bg-peach sticky top-0'>
+      <div className='h-24 flex '>
+        <div className='mr-80 pt-4 pl-20'>
+          <img src='../assets/peter/header/HWA_logo.png ' alt='logo' />
+        </div>
         <div className='absolute top-10 right-96 flex'>
           {navBarItems.map((item, index) => {
             return (
-              <div className='font-DM text-lg font-normal mx-2' key={index}>
+              <div className='font-DM text-lg font-normal mx-4' key={index}>
                 <a href={item.url}>{item.itemName}</a>
               </div>
             );
@@ -22,8 +24,8 @@ const NavBar = () => {
           <div className='font-DM text-lg font-normal mx-2'>Contact</div> */}
         </div>
         <div className='absolute right-5 top-10 flex'>
-          <div className='mx-2'>Search Icon</div>
-          <div className='mx-2'>Donate Button</div>
+          <div className='font-DM text-lg font-normal mx-3'>Search Icon</div>
+          <div className='font-DM text-lg font-normal mx-3'>Donate Button</div>
         </div>
       </div>
       <div>
