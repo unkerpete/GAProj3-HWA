57e2de39e5eb9b4e8728bc01f337b0f96aa0a7e8
unkerpete <peterkjx@gmail.com>
2023-02-05 01:36:32 +0800

    Refactor codes that render navbar items

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import logo from '../assets/peter/header/HWA_logo.png';
-import navBarItems from './navBarItems';
 import { Link } from 'react-router-dom';
 import DrpdwnAboutUs from './DrpdwnAboutUs';
 import DrpdwnOurServices from './DrpdwnOurServices';
@@ -18,6 +17,22 @@ const NavBar = () => {
     theState(false);
   };
 
+  const navBarItems = [
+    { itemName: 'About Us', url: '/about-us', setHover: setHoverAboutUs },
+    {
+      itemName: 'Our Services',
+      url: '/our-services',
+      setHover: setHoverOurServices,
+    },
+    {
+      itemName: 'Get Involved',
+      url: '/get-involved/',
+      setHover: setHoverGetInvolved,
+    },
+    { itemName: 'Current Events', url: '/current-events/', setHover: null },
+    { itemName: 'Contact', url: '/contact-us/', setHover: null },
+  ];
+
   return (
     <div className='bg-peach sticky top-0'>
       <div className='h-24 flex border-y-2 border'>
@@ -25,52 +40,22 @@ const NavBar = () => {
           <img src={logo} alt='logo' width={160} />
         </div>
         <div className='absolute top-10 right-96 flex'>
-          {/* {navBarItems.map((item, index) => {
+          {navBarItems.map((item, index) => {
             return (
-              <div className='font-DM text-lg font-normal mx-4' key={index}>
+              <div
+                className='font-DM text-lg font-normal mx-4'
+                key={index}
+                onMouseEnter={() => {
+                  handleMouseOver(item.setHover);
+                }}
+                onMouseLeave={() => {
+                  handleMouseLeave(item.setHover);
+                }}
+              >
                 <a href={item.url}>{item.itemName}</a>
               </div>
             );
-          })} */}
-          <div
-            className='font-DM text-lg font-normal mx-2 h-16 '
-            onMouseEnter={() => {
-              handleMouseOver(setHoverAboutUs);
-            }}
-            onMouseLeave={() => {
-              handleMouseLeave(setHoverAboutUs);
-            }}
-          >
-            <a href='/about-us'>About Us</a>
-          </div>
-          <div
-            className='font-DM text-lg font-normal mx-2'
-            onMouseEnter={() => {
-              handleMouseOver(setHoverOurServices);
-            }}
-            onMouseLeave={() => {
-              handleMouseLeave(setHoverOurServices);
-            }}
-          >
-            <a href='/our-services'>Our Services</a>
-          </div>
-          <div
-            className='font-DM text-lg font-normal mx-2'
-            onMouseEnter={() => {
-              handleMouseOver(setHoverGetInvolved);
-            }}
-            onMouseLeave={() => {
-              handleMouseLeave(setHoverGetInvolved);
-            }}
-          >
-            <a href='/get-involved'>Get Involved</a>
-          </div>
-          <div className='font-DM text-lg font-normal mx-2'>
-            <a href='/current-events'>Current Events</a>
-          </div>
-          <div className='font-DM text-lg font-normal mx-2'>
-            <a href='/contact-us'>Contact</a>
-          </div>
+          })}
         </div>
         <div className='absolute right-5 top-10 flex'>
           <div className='font-DM text-lg font-normal mx-3'>Search Icon</div>
