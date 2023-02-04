60ee69f0c58aa76256b86a298975a7fa4d183c22
unkerpete <peterkjx@gmail.com>
2023-02-04 15:53:59 +0800

    Draft CSS positioning of elements in NavBar.
    Rough positioning of NavBar elements done. Those elements are also able to bring you to various pages in the site.

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React from 'react';
+import navBarItems from './navBarItems';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <div className='bg-peach'>
+      <div className='h-24 flex'>
+        <div className='mr-80 pt-4 pl-20'>Logo</div>
+        <div className='absolute top-10 right-96 flex'>
+          {navBarItems.map((item, index) => {
+            return (
+              <div className='font-DM text-lg font-normal mx-2' key={index}>
+                <a href={item.url}>{item.itemName}</a>
+              </div>
+            );
+          })}
+          {/* <div className='font-DM text-lg font-normal mx-2'>About Us</div>
+          <div className='font-DM text-lg font-normal mx-2'>Our Services</div>
+          <div className='font-DM text-lg font-normal mx-2'>Get Involved</div>
+          <div className='font-DM text-lg font-normal mx-2'>Current Events</div>
+          <div className='font-DM text-lg font-normal mx-2'>Contact</div> */}
+        </div>
+        <div className='absolute right-5 top-10 flex'>
+          <div className='mx-2'>Search Icon</div>
+          <div className='mx-2'>Donate Button</div>
+        </div>
+      </div>
+      <div>
+        In this position, conditional rendering of a dropdown menu (3 different
+        dropdown menu) when hovering on one of the navbar items. i.e. hover on
+        About-Us will render AboutUs dropdown menu, hover on Our Services will
+        render OurServices dropdown menu
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
