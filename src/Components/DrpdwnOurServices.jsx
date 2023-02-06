f5b360f4bacd999db5ed1bf7a7e4f0fa394734f5
unkerpete <peterkjx@gmail.com>
2023-02-06 10:09:04 +0800

    update navbar, and created 3 child components for navbar

--- src/Components/DrpdwnOurServices.jsx
+++ src/Components/DrpdwnOurServices.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 
 const DrpdwnOurServices = (props) => {
   return (
-    <div
-      className='bg-peach h-52 z-10 absolute w-full delay-200'
-      onMouseEnter={() => {
-        props.handleMouseOver(props.setHover);
-      }}
-      onMouseLeave={() => {
-        props.handleMouseLeave(props.setHover);
-      }}
-    >
-      Our Services Dropdown Menu
-    </div>
+    <>
+      <div
+        className='bg-secondary-200 h-52 z-10 absolute w-full rounded-b-lg'
+        onMouseEnter={() => {
+          props.handleMouseOver(props.setHover);
+        }}
+        onMouseLeave={() => {
+          props.handleMouseLeave(props.setHover);
+        }}
+      >
+        Our Services Dropdown Menu
+      </div>
+      <div className='w-full absolute top-52 h-screen bg-black opacity-50'></div>
+    </>
   );
 };
 
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../assets/peter/header/HWA_logo.png';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import DrpdwnAboutUs from './DrpdwnAboutUs';
 import DrpdwnOurServices from './DrpdwnOurServices';
 import DrpdwnGetInvolved from './DrpdwnGetInvolved';
@@ -10,52 +9,102 @@ const NavBar = () => {
   const [hoverAboutUs, setHoverAboutUs] = useState(false);
   const [hoverOurServices, setHoverOurServices] = useState(false);
   const [hoverGetInvolved, setHoverGetInvolved] = useState(false);
-  const handleMouseOver = (theState) => {
-    theState(true);
+  const [navBarBackgroundColor, setnavBarBackgroundColor] =
+    useState('bg-peach');
+  const [currentPage, setCurrentPage] = useState('Home');
+  const location = useLocation();
+
+  useEffect(() => {
+    setCurrentPage(location.pathname.substring(1)), [location];
+  });
+
+  const getNavBarColor = (currentPage) => {
+    switch (currentPage) {
+      case 'Home':
+        return 'bg-peach';
+      case 'about-us':
+        return 'bg-peach';
+      case 'our-services':
+        return 'bg-secondary-200';
+      case 'get-involved/':
+        return 'bg-primary-200';
+      default:
+        return 'bg-peach';
+    }
   };
-  const handleMouseLeave = (theState) => {
-    theState(false);
+
+  useEffect(() => {
+    setnavBarBackgroundColor(getNavBarColor(currentPage)), [];
+  });
+
+  const handleMouseOver = (hoverState) => {
+    hoverState(true);
+  };
+  const handleMouseLeave = (hoverState) => {
+    hoverState(false);
   };
 
   const navBarItems = [
-    { itemName: 'About Us', url: '/about-us', setHover: setHoverAboutUs },
+    {
+      itemName: 'About Us',
+      url: '/about-us',
+      bgColor: 'bg-peach',
+      setHover: setHoverAboutUs,
+    },
     {
       itemName: 'Our Services',
       url: '/our-services',
+      bgColor: 'bg-secondary-200',
       setHover: setHoverOurServices,
     },
     {
       itemName: 'Get Involved',
       url: '/get-involved/',
+      bgColor: 'bg-primary-200',
       setHover: setHoverGetInvolved,
     },
-    { itemName: 'Current Events', url: '/current-events/', setHover: null },
-    { itemName: 'Contact', url: '/contact-us/', setHover: null },
   ];
 
   return (
-    <div className='bg-peach sticky top-0'>
+    <div className={`${navBarBackgroundColor} sticky top-0`}>
       <div className='h-24 flex border-y-2 border'>
         <div className='mr-80 pt-4 pl-20'>
           <img src={logo} alt='logo' width={160} />
         </div>
-        <div className='absolute top-10 right-96 flex'>
+        <div className='absolute top-10 right-96 flex flex-wrap'>
           {navBarItems.map((item, index) => {
             return (
               <div
-                className='font-DM text-lg font-normal mx-4'
+                className='font-DM text-lg font-normal px-4 h-14 w-26'
                 key={index}
                 onMouseEnter={() => {
                   handleMouseOver(item.setHover);
+                  console.log('before setting color');
+                  setnavBarBackgroundColor(item.bgColor);
+                  console.log('after setting color');
                 }}
                 onMouseLeave={() => {
                   handleMouseLeave(item.setHover);
+                  // setnavBarBackgroundColor(navBarBackgroundColor.prevValue);
+                  // setnavBarBackgroundColor(() => {
+                  //   getNavBarColor(currentPage);
+                  // });
+                }}
+                onClick={() => {
+                  setnavBarBackgroundColor(item.bgColor);
                 }}
               >
                 <a href={item.url}>{item.itemName}</a>
+                <div>o</div>
               </div>
             );
           })}
+          <div className='font-DM text-lg font-normal mx-4'>
+            <a href='/current-events/'>Current Events</a>
+          </div>
+          <div className='font-DM text-lg font-normal mx-4'>
+            <a href='/contact-us/'>Contact</a>
+          </div>
         </div>
         <div className='absolute right-5 top-10 flex'>
           <div className='font-DM text-lg font-normal mx-3'>Search Icon</div>
