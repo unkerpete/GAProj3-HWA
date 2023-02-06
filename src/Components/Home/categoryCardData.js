27d8c97c3639011da2181b24b7c9ec050f3c5c02
cshujun23 <c.shujun23@gmail.com>
2023-02-06 11:34:01 +0800

    Revert "Revert "Homepage hero section""
    This reverts commit 60b850d0ea44cfb3d6e0c6c93873b3ca97bc52e9.

--- src/Components/Home/categoryCardData.js
+++ src/Components/Home/categoryCardData.js
@@ -1,21 +1,21 @@
 const categoryCardData = [
   {
-    img: "",
+    img: "src/Assets/shujun/homepage/catergories/For_Caregivers.png",
     subheader: "For Caregivers",
     body: "We support family caregivers through providing regular quality programmes and services, and uplifts them with a robust support system.",
   },
   {
-    img: "",
+    img: "src/Assets/shujun/homepage/catergories/For_Donors.png",
     subheader: "For Donors",
     body: "Your gift will enable and support individuals with physical challenges as well as their family caregivers through HWA's rehabilitaion, home care, and transport programmes and services.",
   },
   {
-    img: "",
+    img: "src/Assets/shujun/homepage/catergories/For_Volunteers.png",
     subheader: "For Volunteers",
     body: "The service of volunteers contributes significantly to the social reintegration of the physicallyy challenged in our community.",
   },
   {
-    img: "",
+    img: "src/Assets/shujun/homepage/catergories/For_Disabilities.png",
     subheader: "For People with Disabilities",
     body: "HWA's rehabilitation, home care, and transportation services caters to the distinct needs of the physically challenged. No one gets left behind",
   },
