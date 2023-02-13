1a71a42ba5db2d6d78f6b53ed5ddfc66fccf2d3f
cshujun23 <c.shujun23@gmail.com>
2023-02-13 15:15:06 +0800

    Fix NavBar Z-index and start on Current Events page

---
+++ src/Components/Current-Events/TagsDisplay.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Tags from "./Tags";
+
+const TagsDisplay = () => {
+  return (
+    <div>
+      <Tags text="blah" />
+    </div>
+  );
+};
+
+export default TagsDisplay;
--- src/Components/PictureCards.jsx
+++ src/Components/PictureCards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Button from "./Button";
 
 const PictureCards = (props) => {
   const getTagClass = (tag) => {
@@ -33,31 +34,62 @@ const PictureCards = (props) => {
   };
 
   return (
-    <div className="grid grid-cols-3 p-4 gap-16">
+    <div
+      className={`${
+        props.vertical ? "grid grid-rows-3" : "grid grid-cols-3 gap-8"
+      }`}
+    >
       {props.pictureInfo.map((obj, index) => {
         return (
-          <div className="col h-[459px] content-center" key={index}>
-            <img
-              className="rounded-2xl"
-              src={obj.url}
-              alt={obj.alt}
-              width="431"
-              height="287"
-            />
-            <p className={"font-DM text-base font-normal mt-3"}>
-              {obj.tag ? (
-                <Link to={`${getTagLink(obj.tag)}`}>
-                  <span className={`${getTagClass(obj.tag)} `}>{obj.tag}</span>
-                </Link>
-              ) : (
-                <span className="text-white">no tag</span>
-              )}
-            </p>
-            <Link to={obj.link}>
-              <h3 className="py-3">{obj.title}</h3>
-            </Link>
+          <div
+            className={
+              props.vertical
+                ? "flex flex-row my-8"
+                : "col h-[459px] content-center"
+            }
+            key={index}
+          >
+            {props.vertical ? (
+              <p className="font-DM text-3xl font-medium text-primary-800 mr-40">
+                {obj.dateStart}
+              </p>
+            ) : null}
+            <div>
+              <img
+                className={`${
+                  props.vertical ? "mr-7" : "mb-3"
+                } rounded-2xl border `}
+                src={obj.img}
+                width="431"
+                height="287"
+              />
+            </div>
 
-            <p className="font-DM text-xl font-normal">{obj.date}</p>
+            <div>
+              <p className={"font-DM text-base font-normal"}>
+                {obj.tag ? (
+                  <Link to={`${getTagLink(obj.tag)}`}>
+                    <span className={`${getTagClass(obj.tag)} `}>
+                      {obj.tag}
+                    </span>
+                  </Link>
+                ) : (
+                  <span className="text-white">no tag</span>
+                )}
+              </p>
+              <Link to={obj.action}>
+                <h3 className="py-3 w-3/4">{obj.title}</h3>
+              </Link>
+              <p className="font-DM text-xl font-normal">
+                {obj.dateStart} {obj.dateEnd ? "-" : null} {obj.dateEnd},
+                {obj.time}
+              </p>
+              {props.vertical ? (
+                <div className="flex align-bottom">
+                  <Button text="Learn More" link="" />
+                </div>
+              ) : null}
+            </div>
           </div>
         );
       })}
