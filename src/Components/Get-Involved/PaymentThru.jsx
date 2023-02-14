aa29eda48c060002db7a293293a35ccdc6a7c504
eezhar <eezhar@gmail.com>
2023-02-14 18:50:35 +0800

    dummy loading for payment done

--- src/Components/Get-Involved/PaymentThru.jsx
+++ src/Components/Get-Involved/PaymentThru.jsx
@@ -1,28 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button";
+import Loaded from "./Loaded";
+import Loading from "./Loading";
 
 // Need to create Route for payment thru
 const PaymentThru = () => {
+  const [loading, setLoading] = useState(true);
 
-  
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
   return (
     <div className="flex flex-col text-primary-800 text-center mt-16 mb-96 ">
-      {/* <h1>We are processing your payment</h1>
-      <p>Loading Spinner</p> */}
-
-      <h1 className="">Thank you for your donation!</h1>
-      <p className="m-5 font-DM text-xl font-normal">
-        A confirmation has been sent to your email.
-      </p>
-      <img
-        className="object-scale-down h-[445px]"
-        src="src/Assets/izhar/GetInvolved/GetInvolved_image.png"
-        alt=""
-      />
-      <p className="m-5 font-DM text-xl font-normal">
-        In the meantime, you may find out how else you can support us.
-      </p>
-      <Button link="/home" text="Learn More" />
+      {loading && <Loading />}
+      {!loading && <Loaded />}
     </div>
   );
 };
