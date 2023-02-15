1358ca7bf7fa881e43f750591826836c6f1ea113
mfirdausbn <mfirdausbn@gmail.com>
2023-02-15 21:21:40 +0800

    final update. css updated and endpoints working

--- src/Components/Admin/DisplayEvents.jsx
+++ src/Components/Admin/DisplayEvents.jsx
@@ -26,13 +26,17 @@ const DisplayEvents = () => {
   return (
     <>
       <button
-        className="my-5 ml-44 inline-block px-6 py-2.5 bg-primary-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-primary-800 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out"
+        className="my-6 ml-44 inline-block px-6 py-2.5 bg-primary-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-primary-800 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out"
         onClick={handleFetchEvents}
       >
         Fetch Events
       </button>
       {/* Display spinner if still loading */}
-      {isLoading && <img src={spinner} width="200" />}
+      {isLoading && (
+        <div className="ml-40">
+          <img src={spinner} width="200" />
+        </div>
+      )}
 
       {/* Updated mapping */}
       {events.map((event) => {
@@ -45,21 +49,44 @@ const DisplayEvents = () => {
             <img src={`data:image/jpg;base64,${base64string}`} />
             <span className="text-gray-700 mb-4">
               <p>
-                Title :&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                <span className="font-bold">
+                  Title :&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                </span>
                 {event.title}
               </p>
-              <p>Start Date: &nbsp;&nbsp; {event.dateStart.split("T")[0]}</p>
               <p>
-                End Date: &nbsp;&nbsp;&nbsp; &nbsp;{event.dateEnd.split("T")[0]}
+                <span className="font-bold">Start Date: &nbsp;&nbsp;</span>{" "}
+                {event.dateStart.split("T")[0]}
               </p>
               <p>
-                Time: &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                <span className="font-bold">
+                  End Date: &nbsp;&nbsp;&nbsp; &nbsp;
+                </span>
+                {event.dateEnd.split("T")[0]}
+              </p>
+              <p>
+                <span className="font-bold">
+                  Time: &nbsp;&nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                </span>
                 {event.timeString}
               </p>
-              <p>Description: {event.description}</p>
-              <p>Action :&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; {event.action}</p>
-              <p>Tags : &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; {event.tag}</p>
-              <div className="flex justify-center">
+              <p>
+                <span className="font-bold">Description: </span>
+                {event.description}
+              </p>
+              <p>
+                <span className="font-bold">
+                  Action :&nbsp; &nbsp; &nbsp; &nbsp;&nbsp;
+                </span>{" "}
+                {event.action}
+              </p>
+              <p>
+                <span className="font-bold">
+                  Tags : &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp;{" "}
+                </span>
+                {event.tag}
+              </p>
+              <div className="flex justify-center ml-3">
                 <Delete eventId={event._id} />
               </div>
 
@@ -68,7 +95,7 @@ const DisplayEvents = () => {
                   title={event.title}
                   description={event.description}
                   img={event.img}
-                  action={event.img}
+                  action={event.action}
                   tag={event.tag}
                   id={event._id}
                 />
--- src/Components/Admin/Login.jsx
+++ src/Components/Admin/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Login = ( {SET_ACCESS_TOKEN, ACCESS_TOKEN } ) => {
+const Login = ({ SET_ACCESS_TOKEN, ACCESS_TOKEN }) => {
   const [login, setLogin] = useState({
     username: "",
     password: "",
@@ -27,9 +27,7 @@ const Login = ( {SET_ACCESS_TOKEN, ACCESS_TOKEN } ) => {
       .then((result) => {
         console.log(result.access);
         SET_ACCESS_TOKEN(result.access);
-        
       });
-
   };
 
   const createUser = (e) => {
@@ -56,43 +54,47 @@ const Login = ( {SET_ACCESS_TOKEN, ACCESS_TOKEN } ) => {
 
   return (
     <div className="flex justify-center">
-      <div>
-      <h3>Enter Login Details</h3>
-      <form className="m-4" onSubmit={(e) => loginUser(e)}>
-        <input
-          className="border-2 border-black-500"
-          placeholder="username"
-          type="text"
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          value={login.username}
-          name="username"
-        ></input>
-        <br />
-        <input
-          className="border-2 border-black-500"
-          placeholder="password"
-          type="text"
-          onChange={(e) => {
-            handleChange(e);
-          }}
-          value={login.password}
-          name="password"
-        ></input>
-        <br />
-        <button className="bg-gray-200 p-2 rounded" placeholder="submit">
-          Submit
-        </button>
-        <button
-          className="bg-gray-200 p-2 rounded"
-          placeholder="create"
-          onClick={(e) => createUser(e)}
-        >
-          create
-        </button>
-      </form>
-    </div>
+      <div className="rounded-lg shadow-lg bg-primary-200 m-10">
+        <span className="font-DM text-base font-normal">
+          <div className="p-10">
+            <div className="font-DM text-xl font-normal ml-5 w-44">Enter Login Details</div>
+            <form className="m-4" onSubmit={(e) => loginUser(e)}>
+              <input
+                className="border-2 border-lightgray-200 mx-auto mb-2 rounded-lg p-1"
+                placeholder="username"
+                type="text"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                value={login.username}
+                name="username"
+              ></input>
+              <br />
+              <input
+                className="border-2 border-lightgray-200 mx-auto mb-2 rounded-lg p-1"
+                placeholder="password"
+                type="text"
+                onChange={(e) => {
+                  handleChange(e);
+                }}
+                value={login.password}
+                name="password"
+              ></input>
+              <br />
+              <button className="inline-block px-4 mx-2 py-2.5 bg-primary-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-primary-800 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out" placeholder="submit">
+                Submit
+              </button>
+              <button
+                className="inline-block px-4 mx-2 py-2.5 bg-primary-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-primary-800 hover:shadow-lg focus:bg-primary-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-primary-800 active:shadow-lg transition duration-150 ease-in-out"
+                placeholder="create"
+                onClick={(e) => createUser(e)}
+              >
+                create
+              </button>
+            </form>
+          </div>
+        </span>
+      </div>
     </div>
   );
 };
