d169e15693c0c95d919d11055976800aef6f7801
eezhar <eezhar@gmail.com>
2023-02-15 12:51:01 +0800

    edited images to be rendered

--- src/Components/Admin/DisplayEvents.jsx
+++ src/Components/Admin/DisplayEvents.jsx
@@ -1,93 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Delete from "./Delete";
 import Update from "./Update";
 import axios from "axios";
+import useAxios from "../../Hooks/useAxios";
+import { Buffer } from "buffer";
 
 const DisplayEvents = () => {
   const [toggleShowEvents, setToggleShowEvents] = useState(false);
 
-  const [events, setEvents] = useState([
-    // {
-    //   title: "Garden of Hope - Flower planting",
-    //   date: "19 Feb 2023",
-    //   start: "9:00 am",
-    //   end: "11:00 am",
-    //   description: "Lorem ipsum dolor sit amet",
-    //   img: "Lorem ipsum dolor sit",
-    //   action: "Lorem Link",
-    //   tag: "Community Gathering",
-    // },
-    // {
-    //   title: "Medical Talk - Essential Caregiving Skills",
-    //   date: "21 Feb 2023",
-    //   start: "9:00 am",
-    //   end: "11:00 am",
-    //   description: "Lorem ipsum dolor sit amet",
-    //   img: "Lorem ipsum dolor sit",
-    //   action: "Lorem Link",
-    //   tag: "Talks",
-    // },
-    // {
-    //   title: "Seniors Go Digital",
-    //   date: "21 Feb 2023",
-    //   dateEnd: "23 Feb 2023",
-    //   start: "9:00 am",
-    //   end: "5:30 pm",
-    //   description: "Lorem ipsum dolor sit amet",
-    //   img: "Lorem ipsum dolor sit",
-    //   action: "Lorem Link",
-    //   tag: "Classes & Workshops",
-    // },
-  ]);
-
-  // const fetchEvents = async () => {
-  //   const url = "http://127.0.0.1:5001/events/showall";
-  //   const res = await fetch(url);
-  //   const data = await res.json();
-  //   setEvents(data);
-  //   console.log(data);
-  // };
-
-  const mapped = () => {
-    events.map((singleEvent, index) => {
-      console.log(singleEvent);
-    });
-  };
+  const [events, setEvents] = useState([]); // To store the array of events
 
   const fetchEvents = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:5001/events/showall");
-      setEvents(response.data);
-      mapped();
-      console.log(events);
+      const res = await axios.get("http://127.0.0.1:5001/events/showall");
+      setEvents(res.data);
+      // mapped();
+      // console.log("events.data.events: ", events.data.events);
+      console.log("events ", events);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleFetchEvents = async () => {
+    try {
+      const res = await axios.get("http://127.0.0.1:5001/events/showall");
+      setEvents(res.data.events);
+      console.log("events: ", events);
     } catch (err) {
       console.log(err);
     }
   };
 
   const handleToggle = () => {
-    fetchEvents();
     setToggleShowEvents(!toggleShowEvents);
   };
 
   // const mapped = () => {
-  //   events.map((singleEvent, index) => {
-  //     console.log(singleEvent);
+  //   events.map((ele, index) => {
+  //     return <div>{ele.title}</div>;
   //   });
   // };
 
-  // useEffect(()=> {
-  //   handleToggle();;
+  // useEffect(() => {
+  //   mapped();
   // }, [events]);
 
   return (
     <>
       <button
         className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-        onClick={fetchEvents}
+        onClick={handleFetchEvents}
       >
         Fetch Events
       </button>
+
       <button
         className="inline-block px-6 py-2.5 bg-gray-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
         onClick={handleToggle}
@@ -129,16 +96,16 @@ const DisplayEvents = () => {
       )} */}
 
       {/* Updated mapping */}
-      {/* <div className="">
+      <div className="">
         {events.map((event) => {
           const base64string = Buffer.from(event.img.data.data).toString(
             "base64"
           );
           return <img src={`data:image/jpg;base64,${base64string}`} />;
         })}
-      </div> */}
+      </div>
 
-      {/* {mapped} */}
+      {/* {mapped ? mapped : ""} */}
     </>
   );
 };
