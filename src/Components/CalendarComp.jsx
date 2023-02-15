83cc2e22e3b684b041fad1aef90b0c200b0c33ab
cshujun23 <c.shujun23@gmail.com>
2023-02-15 15:49:24 +0800

    Create new calendar component

--- src/Components/CalendarComp.jsx
+++ src/Components/CalendarComp.jsx
@@ -1,18 +1,37 @@
 import { useState } from "react";
 import Calendar from "react-calendar";
+import "../App.css";
 
-function CalendarComp() {
+function App() {
   const [date, setDate] = useState(new Date());
+  const [showTime, setShowTime] = useState(false);
 
   return (
-    <div className="app">
-      <h1 className="header">React Calendar</h1>
-      <div className="calendar-container">
-        <Calendar onChange={setDate} value={date} />
+    <div className="wrapper">
+      <div>
+        <Calendar
+          onChange={setDate}
+          value={date}
+          onClickDay={() => setShowTime(true)}
+        />
       </div>
-      <div className="text-center">Selected date: {date.toDateString()}</div>
+
+      {date.length > 0 ? (
+        <p>
+          <span>Start:</span>
+          {date[0].toDateString()}
+          &nbsp; &nbsp;
+          <span>End:</span>
+          {date[1].toDateString()}
+        </p>
+      ) : (
+        <p>
+          <span>Default selected date:</span>
+          {date.toDateString()}
+        </p>
+      )}
     </div>
   );
 }
 
-export default CalendarComp;
+export default App;
