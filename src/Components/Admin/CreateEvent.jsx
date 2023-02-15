690a225d2afa771747ecf77ae9db6d5d4c266e96
eezhar <eezhar@gmail.com>
2023-02-15 08:30:45 +0800

    updated create event route with API endpoint.  img and data populated in DB

--- src/Components/Admin/CreateEvent.jsx
+++ src/Components/Admin/CreateEvent.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import parse from "date-fns/parse";
+import axios from "axios";
+import { buffer } from "buffer";
 
 const CreateEvent = () => {
   const [toggleCreate, setToggleCreate] = useState(false);
+  const [file, setFile] = useState(null);
 
   const [event, setEvent] = useState({
     title: "abc",
     dateStart: "",
     dateEnd: "",
-    time: "0900",
+    timeString: "0900",
     description: "abc",
     img: "abc",
     action: "abc",
@@ -22,41 +25,77 @@ const CreateEvent = () => {
     });
   };
 
-  const createEvent = (e) => {
+  // const createEvent = (e) => {
+  //   e.preventDefault();
+
+  //   // convert date string to date object
+  //   const body = event;
+  //   console.log(body);
+  //   // 13/02/2023 -> date  object using date-fns
+  //   // date is formatted as "2023-02-14" when clicked on calendar
+  //   body.dateStart = parse(body.dateStart, "yyyy-MM-dd", new Date());
+  //   body.dateEnd = parse(body.dateEnd, "yyyy-MM-dd", new Date());
+  //   console.log(body.dateStart);
+
+  //   fetch("http://127.0.0.1:5001/events/create", {
+  //     method: "PUT",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(body),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((results) => {
+  //       console.log(results);
+  //     })
+  //     .catch((error) => console.error(error));
+
+  //   setEvent({
+  //     title: "",
+  //     dateStart: "",
+  //     dateEnd: "",
+  //     time: "",
+  //     description: "",
+  //     img: "",
+  //     action: "",
+  //     tag: "",
+  //   });
+  // };
+
+  const createEvent = async (e) => {
     e.preventDefault();
+    const formData = new FormData(); // create an empty form data object
 
-    // convert date string to date object
-    const body = event;
-    console.log(body);
-    // 13/02/2023 -> date  object using date-fns
-    // date is formatted as "2023-02-14" when clicked on calendar
-    body.dateStart = parse(body.dateStart, "yyyy-MM-dd", new Date());
-    body.dateEnd = parse(body.dateEnd, "yyyy-MM-dd", new Date());
-    console.log(body.dateStart);
+    // populate the form data object with input data
+    formData.append("title", event.title);
+    formData.append("dateStart", event.dateStart);
+    formData.append("dateEnd", event.dateEnd);
+    formData.append("timeString", event.timeString);
+    formData.append("description", event.description);
+    formData.append("eventImg", file);
+    formData.append("action", event.action);
+    formData.append("tag", event.tag);
 
-    fetch("http://127.0.0.1:5001/events/create", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((results) => {
-        console.log(results);
-      })
-      .catch((error) => console.error(error));
+    // pass the form data object to the server endpoint
+    try {
+      const response = await axios.put(
+        "http://localhost:5001/events/create",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      console.log(response);
+    } catch (err) {
+      console.error(err);
+    }
 
-    setEvent({
-      title: "",
-      dateStart: "",
-      dateEnd: "",
-      time: "",
-      description: "",
-      img: "",
-      action: "",
-      tag: "",
-    });
+    // unable to console log the form data directly. need to deconstruct to view as per below
+    for (const pair of formData.entries()) {
+      console.log(pair[0] + ", " + pair[1]);
+    }
   };
 
   const handleToggle = () => {
@@ -72,8 +111,8 @@ const CreateEvent = () => {
         Create Event
       </button>
       {toggleCreate && (
-        <div class="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
-          <p class="text-gray-700 text-base mb-4">
+        <div className="block p-6 rounded-lg shadow-lg bg-white max-w-sm">
+          <span className="text-gray-700 text-base mb-4">
             <div className="w-1/2">
               <form onSubmit={(e) => createEvent(e)}>
                 <input
@@ -103,9 +142,9 @@ const CreateEvent = () => {
                 />
                 <input
                   placeholder="time"
-                  name="time"
+                  name="timeString"
                   type="text"
-                  value={event.time}
+                  value={event.timeString}
                   onChange={(e) => handleChange(e)}
                 />
                 <input
@@ -115,13 +154,15 @@ const CreateEvent = () => {
                   value={event.description}
                   onChange={(e) => handleChange(e)}
                 />
+
+                {/* TODO: MODIFY THIS */}
                 <input
                   placeholder="img"
                   name="img"
-                  type="text"
-                  value={event.img}
-                  onChange={(e) => handleChange(e)}
+                  type="file"
+                  onChange={(e) => setFile(e.target.files[0])}
                 />
+
                 <input
                   placeholder="action"
                   name="action"
@@ -142,7 +183,7 @@ const CreateEvent = () => {
                 </button>
               </form>
             </div>
-          </p>
+          </span>
         </div>
       )}
     </>
--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -4,25 +4,33 @@ import DisplayEvents from "../Components/Admin/DisplayEvents";
 import Login from "../Components/Admin/Login";
 
 const Admin = () => {
+  // FIXME: Bring this access token states to parent at App.jsx
   const [ACCESS_TOKEN, SET_ACCESS_TOKEN] = useState("");
   const [updateClicked, setUpdateClicked] = useState(false);
 
   return (
     <>
-      {ACCESS_TOKEN === "" ? (
+      {/* {ACCESS_TOKEN === "" ? (
         <Login
           SET_ACCESS_TOKEN={SET_ACCESS_TOKEN}
           ACCESS_TOKEN={ACCESS_TOKEN}
         />
       ) : (
         <div>{console.log("error. please login again")}</div>
-      )}
-      {ACCESS_TOKEN && (
-        <div className="grid grid-cols-2">
-          <div className="col-span-1"><CreateEvent /></div>
-          <div className="col-span-1 mb-10"> <DisplayEvents /></div>
+      )} */}
+
+      {/* FIXME: NEED TO EDIT */}
+      {/* {ACCESS_TOKEN && ( */}
+      <div className="grid grid-cols-2">
+        <div className="col-span-1">
+          <CreateEvent />
         </div>
-      )}
+        <div className="col-span-1 mb-10">
+          {" "}
+          <DisplayEvents />
+        </div>
+      </div>
+      {/* )} */}
     </>
   );
 };
